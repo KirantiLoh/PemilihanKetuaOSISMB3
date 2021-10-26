15dba7ecf37f2f36995cd9b63c333e422eaeab52
Maurice Yang <mauriceyangmy28@gmail.com>
2021-10-26 11:35:52 +0700

    Edited ALLOWED_HOST

--- MB3PemilihanKetosSite/settings.py
+++ MB3PemilihanKetosSite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["mb3-pemilihanketuaosis.herokuapp.com/", "127.0.0.1:8000"]
 
 
 # Application definition
